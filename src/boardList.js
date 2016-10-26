3f33977242064dfae6a7c9df80adf978c7a5d7c8
Piotr Lawniczek <piotr.lawniczek93@gmail.com>
2016-10-26 20:46:42 +0200

    dodanie wyszukiwania tablic

--- src/boardList.js
+++ src/boardList.js
@@ -2,11 +2,35 @@ import { Board } from './board';
 
 export class BoardList {
   boards = [];
-  title= "Board List";
+  title = "Board List";
+  searchText;
+  backup = [];
 
   newBoard(name) {
-      this.boards.push(new Board(name));
-      var text = this.newBoardModalText;
-      text.value = "";
+    this.boards.push(new Board(name));
+    var text = this.newBoardModalText;
+    text.value = "";
+  }
+  boardSearch(searchText) {
+    this.searchText = searchText;
+    let filtered = [];
+    if (this.backup.length === 0) {
+      this.backup = this.boards;
+    }
+    if (this.searchText) {
+
+
+
+      for (var index = 0; index < this.boards.length; index++) {
+        if (this.boards[index].boardName === this.searchText) {
+          filtered.push(this.boards[index]);
+        }
+      }
+      this.boards = filtered;
+    }
+    else {
+      this.boards = this.backup;
+    }
+
   }
 }
